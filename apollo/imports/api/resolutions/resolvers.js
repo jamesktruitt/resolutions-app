e79f40165b74826bfd347afa6d835611077a393f
James Truitt <tru@rockyagirl.com>
2018-07-24 15:25:12 -0600

    created graphql table/schema, new resolutions resolver and rendered on client

---
+++ apollo/imports/api/resolutions/resolvers.js
@@ -0,0 +1,16 @@
+export default {
+  Query: {
+    resolutions() {
+      return [
+        {
+          _id: 'resolution_01',
+          name: 'Get stuff done!'
+        },
+        {
+          _id: 'resolution_02',
+          name: 'Get a job!'
+        }
+      ]
+    }
+  }
+};
--- apollo/imports/startup/server/register-api.js
+++ apollo/imports/startup/server/register-api.js
@@ -1,2 +1,34 @@
-import ResolutionsSchema from '../../api/resolutions/resolutions.graphql';
+import { createApolloServer } from 'meteor/apollo';
+import { makeExecutableSchema } from 'graphql-tools';
+import merge from 'lodash/merge';
 
+import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql';
+import ResolutionsResolvers from '../../api/resolutions/resolvers';
+
+const testSchema = `
+type Query {
+  hi: String
+  resolutions: [Resolution]
+}
+`;
+
+const typeDefs = [ testSchema, ResolutionsSchema ];
+
+const testResolver = {
+  Query: {
+    hi() {
+      return 'Hello James Truitt';
+    }
+  }
+};
+
+const resolvers = merge(testResolver, ResolutionsResolvers);
+
+console.log(resolvers);
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+createApolloServer({ schema });
